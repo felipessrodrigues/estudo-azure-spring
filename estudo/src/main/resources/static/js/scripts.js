87ce15a3412c4564fce161789a1d338bf331f490
Rodrigues <12662592+felipessrodrigues@users.noreply.github.com>
2024-07-29 01:14:30 -0300

    Atualização repo com nova prática

--- estudo/src/main/resources/static/js/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const title = document.getElementById('title');
-
-    title.addEventListener('click', () => {
-        console.log('O título foi clicado!');
-        alert('Você clicou no título!');
-    });
-});
---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#title').on('click', function() {
+        console.log('O título foi clicado!');
+        alert('Você clicou no título!');
+    });
+});
